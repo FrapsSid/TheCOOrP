78830d3bf3389382ac44836ff09beb713d240947
FrapsSid <64778626+FrapsSid@users.noreply.github.com>
2025-06-12 23:37:55 +0300

    Add files via upload

---
+++ resources/agent_resources/agent_res.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name AgentStats
+
+@export var max_hp: int = 100
+@export var current_hp: int = 100
+@export var max_sp: int = 50
+@export var current_sp: int = 50
+
+const ArmorStats = preload("res://resources/agent_resources/ArmorStats.gd")
+const WeaponStats = preload("res://resources/agent_resources/WeaponStats.gd")
+@export var current_armor: ArmorStats = null
+@export var current_weapon: WeaponStats = null
