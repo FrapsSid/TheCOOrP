f8878974f73692c80d2aca1da1f5302ef4e9abe2
FrapsSid <64778626+FrapsSid@users.noreply.github.com>
2025-06-15 16:52:53 +0300

    Add files via upload

--- resources/agent_resources/agent_res.gd
+++ resources/agent_resources/agent_res.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name AgentStats
 
+@export var name: String = "Bong Bong"
 @export var max_hp: int = 100
 @export var current_hp: int = 100
 @export var max_sp: int = 50
@@ -10,3 +11,6 @@ const ArmorStats = preload("res://resources/agent_resources/ArmorStats.gd")
 const WeaponStats = preload("res://resources/agent_resources/WeaponStats.gd")
 @export var current_armor: ArmorStats = null
 @export var current_weapon: WeaponStats = null
+
+@export var current_room: NodePath = NodePath()
+@export var path2d_progress: float = 0.0
